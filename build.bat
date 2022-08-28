7a42598c519c63d190736e119ec4b1e3d5b6d978
GreyStinger <greystinger@gmail.com>
2022-08-28 16:05:13 +0200

    Added standard building to build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-pc-windows-msvc --release
+cargo build --release
 
 upx --best --lzma "target\x86_64-pc-windows-msvc\release\riv.exe"
+upx --best --lzma "target\release\riv.exe"
